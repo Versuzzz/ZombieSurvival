c162a1cb3d5869084970d60ab74ca9fd18bec01e
VerSS <verss560@gmail.com>
2019-06-01 21:22:46 +0300

    first commite

---
+++ Assets/Scripts/ButtonLeft.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonLeft : MonoBehaviour {
+
+    static private int speed = 20;
+    public Transform Hero;
+
+    public void OnMouseDrag()
+    {
+        Vector3 position = Hero.position;
+        Hero.position = Vector2.MoveTowards(Hero.position, new Vector2(position.x, position.y), speed * Time.deltaTime);
+    }
+}
